57b087b5829f7364fb7cdd6e1cca2b6f4c3b7752
incuesta <incuesta8@gmail.com>
2018-02-23 12:14:57 +0800

    Strill fixing Devise production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = 
   {
