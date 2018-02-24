007972571033ac1dc4b78151e056f531c3c31c98
incuesta <incuesta8@gmail.com>
2018-02-24 11:08:16 +0800

    Modified production config for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = 
   {
