e5b4451bc45f4ece1cfbe194f97dc271c06531b0
incuesta <incuesta8@gmail.com>
2018-03-17 16:18:06 +0800

    figuring out backups

--- app/models/idol.rb
+++ app/models/idol.rb
@@ -2,4 +2,11 @@ class Idol < ApplicationRecord
 		has_many :talents, dependent: :destroy
 
 		validates :last_name, presence: true
+
+
+
+		def run_a_cmd
+			cmd = 'heroku pg:backups:capture --app alpha-web-app'
+			system cmd
+		end
 end
